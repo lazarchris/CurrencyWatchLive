a9fdde76078f46dec9936d1e16b20fecf1e4b8d6
Christy Lazar <anish.monachan@quanthouse.com>
2024-02-09 15:21:25 +0100

    terraform

--- terraform/virtual_machine.tf
+++ terraform/virtual_machine.tf
@@ -1,3 +1,9 @@
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vm-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+}
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "fsm-machine"
