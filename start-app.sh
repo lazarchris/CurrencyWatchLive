954f018d3bcd7b4691663fd8b521e497e56c929c
Christy Lazar <christylazarac@gmail.com>
2024-02-11 11:25:19 +0100

    update dependencies

--- start-app.sh
+++ start-app.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Check if venv exists and delete it if it does
-if [ -d "venv" ]; then
-    echo "Deleting existing venv..."
-    rm -rf venv
-fi
-
-# Create and activate a new virtual environment
-echo "Creating new venv..."
-python3 -m venv venv
-source venv/bin/activate
-
 # Run Python script
 echo "Running Python app..."
 python3 ./app/main.py 
